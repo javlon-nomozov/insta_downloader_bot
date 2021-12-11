f844163725a424f76177c3a62daca96e749721dc
JBN-23 <69708014+JBN-23@users.noreply.github.com>
2021-12-11 23:22:49 +0500

    refresh

--- data/config.py
+++ data/config.py
@@ -1,28 +1,28 @@
-from environs import Env
-
-# environs kutubxonasidan foydalanish
-env = Env()
-env.read_env()
-
-# .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-IP = env.str("ip")  # Xosting ip manzili
-CHANNELS = env.list("CHANNELS")
-GROUPS = env.list("GROUPS")
+# from environs import Env
 #
-# import os
+# # environs kutubxonasidan foydalanish
+# env = Env()
+# env.read_env()
+#
+# # .env fayl ichidan quyidagilarni o'qiymiz
+# BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+# ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+# IP = env.str("ip")  # Xosting ip manzili
+# CHANNELS = env.list("CHANNELS")
+# GROUPS = env.list("GROUPS")
 # #
-# #
-# # # .env fayl ichidan quyidagilarni o'qiymiz
-# #
-# BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))
-# # ADMINS = list(os.environ.get("ADMINS"))
-# # # IP = str(os.environ.get("ip"))
-# # CHANNELS = list(os.environ.get("CHANNELS"))
-# # GROUPS = ["@gdfnjdu"]
+import os
 #
 #
+# # .env fayl ichidan quyidagilarni o'qiymiz
+#
+BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))
+# ADMINS = list(os.environ.get("ADMINS"))
+# # IP = str(os.environ.get("ip"))
+# CHANNELS = list(os.environ.get("CHANNELS"))
 # GROUPS = ["@gdfnjdu"]
-# CHANNELS = ['-1001678147207']
-# ADMINS = ['1559808421']
+
+
+GROUPS = ["@gdfnjdu"]
+CHANNELS = ['-1001678147207']
+ADMINS = ['1559808421']
